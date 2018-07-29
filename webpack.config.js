bc39f1c0986f6e8189237228b641ea040bb70f22
Steven Isbell <steveni_92@msn.com>
2018-07-29 08:05:26 -0500

    webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 const config = {
-  entry: path.join(__dirname, '/static/js'),
+  entry: path.join(__dirname, '/src'),
   output: {
-    path: path.join(__dirname, '/static/dist'),
+    path: path.join(__dirname, '/dist'),
     filename: 'bundle.js'
   },
   resolve: {
@@ -20,11 +23,25 @@ const config = {
     ]
   },
   devServer: {
-    contentBase: path.join(__dirname, '/static/dist'),
+    contentBase: path.join(__dirname, 'src'),
     compress: true,
     port: 3000
   },
-  devtool: 'inline-source-map',
+  plugins: [
+    // A webpack plugin to remove/clean your build folder(s) before building
+    new CleanWebpackPlugin(['dist']),
+    // Simplifies creation of HTML files to serve your webpack bundles
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      filename: './index.html'
+    }),
+    // This plugin extracts CSS into separate files. It creates a CSS file per JS file which contains CSS.
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  devtool: 'source-map',
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 };
 

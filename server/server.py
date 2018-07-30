ea5106014e299fddec35a8d7f2a0407d308c586d
Steven Isbell <steveni_92@msn.com>
2018-07-29 21:14:19 -0500

    replace with cra

--- server/server.py
+++ server/server.py
@@ -1,20 +1,21 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, send_from_directory
 import random
 
 app = Flask(__name__, static_folder="../dist",
             template_folder="../dist")
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route('/')
+def serve():
+    return send_from_directory('../dist', 'index.html')
 
 
-@app.route("/hello")
+@app.route("/api/hello")
 def get_hello():
     greeting_list = ["Hi", "Hello", "Good To See You", "Welcome", "Hi There"]
     return random.choice(greeting_list)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(use_reloader=True, port=5000, threaded=True)
